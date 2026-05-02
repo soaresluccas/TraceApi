9898f4f33e111df8a7587778430d8eca549b7517
soaresluccas <lucaspsoares.wav@gmail.com>
2026-05-02 15:09:04 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Servidor iniciado na porta ${PORT}`);
-  console.log(`📚 Documentação disponível em http://localhost:${PORT}/api-docs`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`✅ Servidor iniciado na porta ${PORT}`);
+    console.log(`📚 Documentação disponível em http://localhost:${PORT}/api-docs`);
+  });
+}
+
+export default app;
 
